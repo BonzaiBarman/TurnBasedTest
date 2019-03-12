316477b44ee9aec0c09d9249ee7d395c13309b84
BonzaiBarman <46822088+BonzaiBarman@users.noreply.github.com>
2019-03-12 15:34:13 -0600

    tut step 16

---
+++ Assets/Scripts/SampleUnit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleUnit : Unit
+{
+	public Color LeadingColor;
+	public override void Initialize()
+	{
+		base.Initialize();
+		transform.position += new Vector3(0,1,0);
+		GetComponent<Renderer>().material.color = LeadingColor;
+	}
+	public override	void MarkAsFriendly()
+	{
+		GetComponent<Renderer>().material.color = LeadingColor + new Color(0.8f, 1, 0.8f);
+	}
+	public override	void MarkAsReachableEnemy()
+	{
+		GetComponent<Renderer>().material.color = LeadingColor + Color.red;
+	}
+	public override	void MarkAsSelected()
+	{
+		GetComponent<Renderer>().material.color = LeadingColor + Color.green;
+	}
+	public override	void UnMark()
+	{
+		GetComponent<Renderer>().material.color = LeadingColor + LeadingColor;
+	}
+}
